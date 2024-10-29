a0a3036ce6108a2543a698ae2342b0330eaf4e1c
myunglyu <180991957+myunglyu@users.noreply.github.com>
2024-10-29 16:34:06 -0700

    modified:   AspNetCoreTodo/AspNetCoreTodo.csproj 	modified:   AspNetCoreTodo/Controllers/TodoController.cs 	modified:   AspNetCoreTodo/Data/ApplicationDbContext.cs 	new file:   AspNetCoreTodo/Data/Migrations/20241029231045_AddItems.Designer.cs 	new file:   AspNetCoreTodo/Data/Migrations/20241029231045_AddItems.cs 	modified:   AspNetCoreTodo/Data/Migrations/ApplicationDbContextModelSnapshot.cs 	modified:   AspNetCoreTodo/Program.cs 	deleted:    AspNetCoreTodo/Services/FakeTodoItemService.cs 	modified:   AspNetCoreTodo/Services/ITodoItemService.cs 	new file:   AspNetCoreTodo/Services/TodoItemService.cs 	modified:   AspNetCoreTodo/Views/Shared/_Layout.cshtml 	new file:   AspNetCoreTodo/Views/Todo/AddItemPartial.cshtml 	modified:   AspNetCoreTodo/Views/Todo/index.cshtml 	modified:   AspNetCoreTodo/wwwroot/js/site.js

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Data;
@@ -8,5 +9,13 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<TodoItem> Items { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // 
     }
 }
